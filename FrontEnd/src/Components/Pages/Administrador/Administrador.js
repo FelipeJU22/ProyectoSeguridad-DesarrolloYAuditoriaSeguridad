c5aea678d93b49c9311f4fc00650e6b4eecc2df4
Ricardo Borbón Mena <ricardobm@estudiantec.cr>
2026-04-09 15:50:44 -0600

    refactor: change some folder´s names

--- FrontEnd/src/Components/Pages/Administrador/Administrador.js
+++ FrontEnd/src/Components/Pages/Administrador/Administrador.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 function Administrador() {
   return (
-    <div style={{ padding: '40px', color: '#102a43' }}>
+    <div style={{ padding: '40px', color: '#e6ebf0' }}>
       <h1>Panel de Administrador</h1>
       <p>Has ingresado correctamente como administrador.</p>
         <p>Esto lo puse por si acaso, no sé si será necesario. Pero mejor tenerlo y no usarlo, que necesitarlo y no tenerlo XD.</p>
+        <p>No pero hermano de verdad la novia y el trabajo lo tienen tan cooked que ahora hasta paginas en blanco hace.</p>
     </div>
   );
 }
